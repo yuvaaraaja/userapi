c25853d298ab350b963de7e6cf2270cc9a0a7639
yuvaaraaja <yuvaraj2295@gmail.com>
2020-05-25 23:33:13 +0530

    removed logging as it was causing security vulnerabilities. Will add it later

--- userapiweb/src/main/java/com/api/controller/UserController.java
+++ userapiweb/src/main/java/com/api/controller/UserController.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,12 +22,9 @@ public class UserController {
 	@Autowired
 	UserDAO  userDAO ;
 	
-	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-	
 	@RequestMapping(value = UserRestURIConstants.DUMMY_USER, method = RequestMethod.GET)
 	public @ResponseBody User getDummyUser(HttpServletResponse response) throws IOException {
 		User user ;
-		logger.info("Start getDummyUser");
 		if(userDAO.listByID("dummy") != null) {
 			user = userDAO.listByID("dummy");
 		}
@@ -45,26 +40,22 @@ public class UserController {
 	
 	@RequestMapping(value = UserRestURIConstants.GET_USER, method = RequestMethod.GET)
 	public @ResponseBody User getUserById(@PathVariable("user_id") String user_id) {
-		logger.info("Start getEmployee. ID="+user_id);
 		return userDAO.listByID(user_id);
 	} 
 	
 	@RequestMapping(value = UserRestURIConstants.GET_ALL_USER, method = RequestMethod.GET)
 	public @ResponseBody List<User> getAllUsers() {
-		logger.info("Start getAllUsers.");
 		return userDAO.listAll();
 	}
 	
 	@RequestMapping(value = UserRestURIConstants.CREATE_USER, method = RequestMethod.POST)
 	public @ResponseBody User createUser(@RequestBody User user) {
-		logger.info("Start createUser.");
 		userDAO.save(user);
 		return user;
 	}
 	
 	@RequestMapping(value = UserRestURIConstants.UPDATE_USER, method = RequestMethod.PUT)
 	public @ResponseBody User updateUser(@RequestBody User newUser) {
-		logger.info("Start updateUser.");
 		User user = userDAO.listByID(newUser.getUser_id());
 		user.setFirstname((newUser.getFirstname() == null || newUser.getFirstname().isEmpty()) ? user.getFirstname() : newUser.getFirstname());
 		user.setLastname((newUser.getLastname() == null || newUser.getLastname().isEmpty()) ? user.getLastname() : newUser.getLastname());
@@ -74,7 +65,6 @@ public class UserController {
 	
 	@RequestMapping(value = UserRestURIConstants.DELETE_USER, method = RequestMethod.DELETE)
 	public @ResponseBody User deleteUser(@PathVariable("user_id") String user_id) {
-		logger.info("Start deleteUser.");
 		User user = userDAO.listByID(user_id);
 		userDAO.delete(user_id);
 		return user;
